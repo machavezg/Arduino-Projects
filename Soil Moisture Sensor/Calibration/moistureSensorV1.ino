e41eec6a57483e1975d94ef1c952b0a7560fc78d
Miguel <machavezg@icloud.com>
2018-01-10 23:19:40 -0800

    Update moistureSensorV1.ino
    updated code for particle

--- Soil Moisture Sensor/Calibration/moistureSensorV1.ino
+++ Soil Moisture Sensor/Calibration/moistureSensorV1.ino
@@ -3,12 +3,14 @@ Calibration Code
 Source: https://www.dfrobot.com/wiki/index.php/Capacitive_Soil_Moisture_Sensor_SKU:SEN0193#Tutorial
 */
 
+#define SENSOR_PIN      A0
+#define DELAY           100
 void setup() {
   Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
 }
 void loop() {
   int val;
-  val = analogRead(0); //connect sensor to Analog 0
-  Serial.print(val); //print the value to serial port
-  delay(100);
+  val = analogRead(SENSOR_PIN); //connect sensor to Analog 0
+  Serial.println(val); //print the value to serial port
+  delay(DELAY);
 }
